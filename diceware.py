40b51a625c70533672b013bf80058dc1eee78a94
Ollie Shadbolt <ollieshadbolt@gmail.com>
2020-08-27 23:40:48 +0100

    Update diceware.py

--- diceware.py
+++ diceware.py
@@ -1,9 +1,9 @@
 from secrets import randbelow
 
 with open('beale.wordlist.asc.txt') as beale_wordlist_asc_txt:
-    words = beale_wordlist_asc_txt.readlines()
+    wordlist = beale_wordlist_asc_txt.readlines()
 
-wordlist = [_[6:-1] for _ in words[2:-10]]
+wordlist = [_[6:-1] for _ in wordlist[2:-10]]
 
 while True:
     input(' '.join(wordlist[randbelow(len(wordlist))] for _ in range(7)))
